a81491864186b2613612fc3b52a39e03f18b351b
maxrichards33 <31513792+maxrichards33@users.noreply.github.com>
2017-09-25 09:08:16 -0600

    fixed my workspace

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//explicit type declaration with no value
+var ramen :String
+//implicittype declaration with a value to assign
+var soup = "ramen"
+ramen = soup
+print(ramen)
+print(soup)
+var myAge :Int
+myAge = 72
+print(myAge)
+myAge = myAge / 3
+myAge = myAge * 3
+myAge = myAge + 57673
+var someValue = 3.1342 * 56
+someValue = someValue * 231.3
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "max"
+print("my name is "+myName)
+print("my age is"); print(myAge)
